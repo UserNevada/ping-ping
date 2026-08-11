04cdf5d6e88dfdf732a661595e68560c50b8ab68
Nevada <migatosellamakiko@proton.me>
2026-08-10 23:16:00 -0400

    It's alive!

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # ping
 Personal Discord Bot that pings a role when someone joins a voice channel.
 
-- [ ] Add discord.py
-- [ ] Go online
+- [x] Add discord.py
+- [x] Go online
 - [ ] When entering voice channel
     - [ ] Log
     - [ ] Send message
--- src/ping/__init__.py
+++ src/ping/__init__.py
@@ -1,2 +1,31 @@
+import discord
+from dotenv import load_dotenv
+from os import getenv
+
+
+load_dotenv()
+BOT_TOKEN = getenv("DISCORD_BOT_TOKEN")
+INTENTS = discord.Intents.default()
+
+
+class MyClient(discord.Client):
+    async def on_ready(self) -> None:
+        print(f"Logged on as {self.user}!")
+
+
+def run_client() -> None:
+    client = MyClient(intents=INTENTS)
+
+    if not BOT_TOKEN is None:
+        client.run(BOT_TOKEN)
+    else:
+        raise ValueError("DISCORD_BOT_TOKEN environment variable is none")
+
+
 def main() -> None:
     print("Hello from ping!")
+    run_client()
+
+
+if __name__ == "__main__":
+    main()
